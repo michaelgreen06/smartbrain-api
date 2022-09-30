ff74b3c113b6f8debdd90fcdc6be8ef84cfe0cce
Michael Green <jonnyblu1982@gmail.com>
2022-09-30 15:17:23 -0600

    changed host to connectionString

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image=require('./controllers/image');
 const db=knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL,//postgresql-trapezoidal-62911
+    connectionString : process.env.DATABASE_URL,//postgresql-trapezoidal-62911
     ssl:true
   }
 });
